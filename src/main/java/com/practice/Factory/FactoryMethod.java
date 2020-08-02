00db28aac8fdf5ab8b618ac992f7e24b769233b3
Peiding Wang <peidingw@amazon.com>
2020-08-02 15:07:17 -0700

    move factory method into a static inner class

--- src/main/java/com/practice/Factory/FactoryMethod.java
+++ src/main/java/com/practice/Factory/FactoryMethod.java
@@ -8,22 +8,24 @@ class Point {
         this.y = y;
     }
 
-    public static Point newCartesianPoint(double x, double y) {
-        return new Point(x, y);
-    }
+    // use static inner class
+    public static class Factory {
+        public static Point newCartesianPoint(double x, double y) {
+            return new Point(x, y);
+        }
 
-    public static Point newPolarPoint(double rho, double theta) {
-        return new Point(
-                rho * Math.cos(theta),
-                rho * Math.sin(theta)
-        );
+        public static Point newPolarPoint(double rho, double theta) {
+            return new Point(
+                    rho * Math.cos(theta),
+                    rho * Math.sin(theta)
+            );
+        }
     }
 }
 
 public class FactoryMethod {
     public static void main(String[] args) {
-        Point point = Point.newPolarPoint(2, 3);
-        Point pointC = Point.newCartesianPoint(3.4, 5.4);
+        Point point = Point.Factory.newCartesianPoint(3.4, 5.6);
 
     }
 }
